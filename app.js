20aae63fadc8fa5d89922349ce4cb21971e4419c
John Bull <jackbull328@gmail.com>
2017-01-01 17:28:24 -0800

    added more stormpath vars

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ var express = require('express'),
 //app config
 app.use(express.static("public"));
 app.use(stormpath.init(app, {
-  application: process.env.STORMPATH_URL,
+  apiKeyFile: '/.stormpath/apiKey.properties',
+  apiKeyId:     process.env.STORMPATH_API_KEY_ID,
+  apiKeySecret: process.env.STORMPATH_API_KEY_SECRET,
+  secretKey:    process.env.STORMPATH_SECRET_KEY,
+  application:  process.env.STORMPATH_URL,
 }));
 app.set("view engin", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
