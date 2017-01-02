15b5a200be9556f50a1a97ad9aee41216c87bee1
John Bull <jackbull328@gmail.com>
2017-01-01 16:34:09 -0800

    change in app order

--- app.js
+++ app.js
@@ -5,24 +5,22 @@ var express = require('express'),
     stormpath = require('express-stormpath');
 
 //app config
-mongoose.connect("mongodb:localhost/crm_dev");
-
-app.set("view engin", "ejs");
 app.use(express.static("public"));
+app.set("view engin", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(stormpath.init(app, {
   application: process.env.STORMPATH_URL,
 }));
 
-//mongoose/model config
-var crmSchema = mongoose.Schema({
-  cusName: String,
-  cusEmail: String,
-  cusNotes: String,
-  created: {type: Date, default: Date.now}
-});
-
-var Customer = mongoose.model("Customer", crmSchema);
+// //mongoose/model config
+// var crmSchema = mongoose.Schema({
+//   cusName: String,
+//   cusEmail: String,
+//   cusNotes: String,
+//   created: {type: Date, default: Date.now}
+// });
+//
+// var Customer = mongoose.model("Customer", crmSchema);
 
 //Rest Routes
 app.get("/", function(req, res) {
