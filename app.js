13d4fb0bb1e1ff4ef05fda1ddb77756bc6908b34
John T. Bull <jackbull328@gmail.com>
2017-01-05 22:33:00 -0800

    added new connection code mongo lab

--- app.js
+++ app.js
@@ -19,11 +19,20 @@ app.use(stormpath.init(app, {
 app.set("view engin", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
 
-// //mongoose/model config
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse.
+  db = database;
+  console.log("Database connection ready");// //mongoose/model config
 var mongdbUri = 'mongodb://<dbuser>:<dbpassword>@ds151048.mlab.com:51048/heroku_2vbj6xl4';
-mongoose.connect(mongodbUri);
-var db = mongoose.connection;
 // var crmSchema = mongoose.Schema({
 //   cusName: String,
 //   cusEmail: String,
