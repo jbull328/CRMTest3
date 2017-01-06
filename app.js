4244c248ec85b00347bc734d2e0586c15fd87719
John T. Bull <jackbull328@gmail.com>
2017-01-05 22:12:16 -0800

    added new customer and semantic UI

--- app.js
+++ app.js
@@ -41,7 +41,11 @@ app.get("/", function(req, res) {
 });
 
 app.get("/userInfo", function(req, res) {
-  res.send("This will be the custom data form, for users to enter thier info. Cant add customer until this iset.")
+  res.send("This will be the custom data form, for users to enter thier info. Cant add customer until this iset.");
+});
+
+app.get("/newCustomer", function(req, res) {
+  res.send("This will be the new customer form!");
 });
 //app launch
 app.listen(process.env.PORT || 3000, function() {
