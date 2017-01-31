ea7f344922fb656070d86e3ea82a1786271c6491
John <jackbull328@gmail.com>
2017-01-31 13:29:10 -0800

    added .env to allow heroku local web

--- app.js
+++ app.js
@@ -2,59 +2,107 @@ var express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose'),
-    stormpath = require('express-stormpath'),
-    mongodb = require('mongodb');
+    stormpath = require('express-stormpath');
 
-var ObjectID = mongodb.ObjectID;
 
 //app config
-app.use(express.static("public"));
+app.set('views', __dirname + '/views');
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/public'));
 app.use(stormpath.init(app, {
   apiKeyFile: '/.stormpath/apiKey.properties',
-  apiKeyId:     process.env.STORMPATH_API_KEY_ID,
-  apiKeySecret: process.env.STORMPATH_API_KEY_SECRET,
-  secretKey:    process.env.STORMPATH_SECRET_KEY,
-  application:  process.env.STORMPATH_URL,
+  apiKeyId:     process.env.STORMPATH_API_KEY_ID || 'key',
+  apiKeySecret: process.env.STORMPATH_API_KEY_SECRET || 'secret',
+  secretKey:    process.env.STORMPATH_SECRET_KEY || 'key',
+  application:  process.env.STORMPATH_URL || 'url',
 }));
-app.set("view engin", "ejs");
+
+
+
 app.use(bodyParser.urlencoded({extended: true}));
 
-// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
-var db;
+var mongodbUri = "mongodb://heroku_2vbj6xl4:713tteam22ns19hkqj90ioeeuc@ds151048.mlab.com:51048/heroku_2vbj6xl4";
 
-// Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
+mongoose.connect(mongodbUri);
 
-  // Save database object from the callback for reuse.
-  db = database;
-  console.log("Database connection ready");// //mongoose/model config
-// var crmSchema = mongoose.Schema({
-//   cusName: String,
-//   cusEmail: String,
-//   cusNotes: String,
-//   created: {type: Date, default: Date.now}
-// });
-//
-
-
-// var Customer = mongoose.model("Customer", crmSchema);
-
+var db = mongoose.connection;
+var organizationSchema = new mongoose.Schema({
+  orgName : String,
+  orgId : String,
+  givenName : String,
+  surname : String,
+});
+var Organization = mongoose.model("Organization", organizationSchema);
+//customer Schema
+var customerSchema = new mongoose.Schema({
+  cusFirstName : String,
+  cusLastName : String,
+  cusAddress : String,
+  cusEmail : String,
+});
+var Customer = mongoose.model("Customer", customerSchema);
 //Rest Routes
 app.get("/", function(req, res) {
-  res.redirect('/login');
+  res.render("landing");
 });
 
-app.get("/userInfo", function(req, res) {
-  res.send("This will be the custom data form, for users to enter thier info. Cant add customer until this iset.");
+app.get("/customerIndex/:id",stormpath.loginRequired, function(req, res) {
+    Organization.findById(req.params.orgId, function(err, foundOrganization) {
+        if(err) {
+            res.redirect("/");
+        } else {
+            res.render("customerIndex", {organization: foundOrganization});
+        }
+    });
 });
 
-app.get("customers/newCustomer", function(req, res) {
-  res.send("This will be the new customer form!");
+app.get("/userNew",stormpath.loginRequired, function(req, res) {
+  res.render("userNew")
 });
+
+app.get('/customers/new', stormpath.loginRequired, function(req, res) {
+  res.render("newCustomer");
+});
+
+app.post('/userNew', stormpath.loginRequired, function(req, res) {
+  var orgName = req.body.orgName;
+  // TODO user regex to remove spaces and capitals from orgId
+  var orgId = req.body.orgName;
+  var givenName = req.body.givenName;
+  var surname = req.body.surname;
+  var newOrganization = {orgName: orgName, orgId: orgId, givenName: givenName, surname: surname,};
+  //create a new campground and save to DB
+  Organization.create(newOrganization, function(err, newlyCreated) {
+   if(err) {
+       console.log(err);
+   } else {
+      res.redirect("/customerIndex/" + orgId);
+   }
+ });
+});
+
+app.post('/newCustomer', stormpath.loginRequired, function(req, res) {
+  var cusFirstName = req.body.cusFirstName;
+  var cusLastName = req.body.cusLastName;
+  var cusAddress = req.body.cusAddress;
+  var cusEmail = req.body.cusEmail;
+  var orgId = Organization.findById(req.params.orgId, function(err, foundOrgId) {
+    if(err) {
+      console.log(err);
+    } else {
+    orgId = {orgId: foundOrgId};
+    }
+  });
+  var newCustomer = {cusFirstName: cusFirstName, cusLastName: cusLastName, cusAddress: cusAddress, cusEmail: cusEmail,}
+  Customer.create(newCustomer, function(err, newlyCreatedCust) {
+    if(err) {
+      console.log(err);
+    } else {
+      res.redirect("/customerIndex/", {organization: foundOrganization});
+    }
+  });
+});
+
 //app launch
 app.listen(process.env.PORT || 3000, function() {
   console.log("CRM Server is Running!");
---
+++ public/scripts/newCustomerSendData.js
@@ -0,0 +1,17 @@
+// document.ready(function() {
+// var organizationName = document.getElementByName('companyName');
+// var oganizationID = document.getElementByName('companyId')
+//
+//   var newOrganization = {
+//   name: organizationName,
+//   nameKey: organizationId
+// };
+//
+// client.createOrganization(newOrganization, function (err, organization) {
+//   if (err) {
+//     return console.log(err);
+//   }
+//
+//   console.log(organization);
+// });
+// });
